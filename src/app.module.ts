ac06c875273d070fa99456aae59c020434db45cb
Antonio Mata <hildeamp@gmail.com>
2019-12-31 15:35:20 -0600

    :recycle: remove middleware from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import { ValidateSearchMiddleware } from './news/middleware/validate-search.middleware';
-import { ValidateSourceParamMiddleware } from './news/middleware/validate-source-param.middleware';
 import { ValidateJsonMiddleware } from './common/middleware/validate-json.middleware';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { NewsModule } from './news/news.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { LoginModule } from './login/login.module';
-import { ValidateUuidMiddleware } from './common/middleware/validate-uuid.middleware';
 
 @Module({
   imports: [
@@ -35,8 +32,6 @@ import { ValidateUuidMiddleware } from './common/middleware/validate-uuid.middle
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
-    consumer.apply(ValidateSearchMiddleware, ValidateSourceParamMiddleware).forRoutes('news');
     consumer.apply(ValidateJsonMiddleware).forRoutes('login', 'users');
-    consumer.apply(ValidateUuidMiddleware).forRoutes('users');
   }
 }
