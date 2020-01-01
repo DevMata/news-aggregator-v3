f4e39feb22893de76deb076e21766433fb265f8a
Antonio Mata <hildeamp@gmail.com>
2019-12-31 22:50:21 -0600

    :recycle: fix naming for users to articles service

--- src/users_to_articles/users-to-articles.module.ts
+++ src/users_to_articles/users-to-articles.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersToArticles } from './entities/userstoarticles.entity';
-import { UsersToArticlesService } from './userstoarticles.service';
+import { UsersToArticlesService } from './users-to-articles.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UsersToArticles])],
