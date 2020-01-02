0d2a1715bbff6859d32e2539cfe5724b865e1fa5
Antonio Mata <hildeamp@gmail.com>
2020-01-01 21:49:34 -0600

    :recycle: Apply new UserRepository

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { HashHelper } from 'src/common/hash.helper';
 import { ArticlesModule } from '../articles/articles.module';
 import { UsersToArticlesModule } from '../users_to_articles/users-to-articles.module';
+import { UserRepository } from './repositories/user-repository.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), ArticlesModule, UsersToArticlesModule],
+  imports: [TypeOrmModule.forFeature([UserRepository]), ArticlesModule, UsersToArticlesModule],
   providers: [UsersService, HashHelper],
   controllers: [UsersController],
   exports: [UsersService],
