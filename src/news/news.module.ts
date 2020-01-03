c1ce8b73260b102871d9485c8dcb2b72d15d3efc
Antonio Mata <hildeamp@gmail.com>
2020-01-03 14:18:43 -0600

    :recycle: inject service for serialization

--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -1,10 +1,21 @@
 import { Module, HttpModule } from '@nestjs/common';
 import { NewsController } from './news.controller';
 import { NewsService } from './news.service';
+import { StringHelper } from './helpers/string-handler.helper';
+import { SerializerHelper } from './helpers/serializer.helper';
+import { NewsConstants } from './news.constants';
 
 @Module({
   controllers: [NewsController],
   imports: [HttpModule],
-  providers: [NewsService],
+  providers: [
+    NewsService,
+    StringHelper,
+    SerializerHelper,
+    {
+      provide: 'Consts',
+      useValue: NewsConstants,
+    },
+  ],
 })
 export class NewsModule {}
