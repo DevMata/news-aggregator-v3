a88d5498b0fcf2e396f1dc680b256cfb2450120d
Antonio Mata <hildeamp@gmail.com>
2020-01-03 14:18:26 -0600

    :recycle: apply service for serialization

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -2,13 +2,17 @@ import { Injectable, HttpService, BadRequestException, ServiceUnavailableExcepti
 import { ConfigService } from '@nestjs/config';
 import { Observable, merge, throwError } from 'rxjs';
 import { map, reduce, catchError } from 'rxjs/operators';
-import { SerializeNewYorkNew, SerializeGuardianNew, SerializeNewsApiNew } from './helpers/serialize.helper';
 import { New } from './interfaces/New.interface';
 import { NewsConstants as cts } from './news.constants';
+import { SerializerHelper } from './helpers/serializer.helper';
 
 @Injectable()
 export class NewsService {
-  constructor(private readonly httpService: HttpService, private readonly configService: ConfigService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+    private readonly serializerHelper: SerializerHelper,
+  ) {}
 
   switchSourceSearch(q: string, source: string): Observable<New[]> {
     switch (source.toLowerCase()) {
@@ -33,7 +37,7 @@ export class NewsService {
     const NYT_KEY = this.configService.get<string>('NYT_KEY');
 
     return this.httpService.get(`${cts.NYT_URL}q=${searchTerm}&api-key=${NYT_KEY}${cts.NYT_FILTERS}`).pipe(
-      map(res => res.data.response.docs.map(SerializeNewYorkNew)),
+      map(res => res.data.response.docs.map(this.serializerHelper.SerializeNewYorkNew)),
       catchError(() => {
         return throwError(new ServiceUnavailableException('News API does not respond'));
       }),
@@ -46,7 +50,7 @@ export class NewsService {
     return this.httpService
       .get(`${cts.GUARDIAN_URL}?api-key=${GUARDIAN_KEY}&q=${searchTerm}${cts.GUARDIAN_FILTERS}`)
       .pipe(
-        map(res => res.data.response.results.map(SerializeGuardianNew)),
+        map(res => res.data.response.results.map(this.serializerHelper.SerializeGuardianNew)),
         catchError(() => {
           return throwError(new ServiceUnavailableException('News API does not respond'));
         }),
@@ -57,7 +61,7 @@ export class NewsService {
     const NEWS_KEY = this.configService.get<string>('NEWS_KEY');
 
     return this.httpService.get(`${cts.NEWS_URL}apiKey=${NEWS_KEY}&q=${searchTerm}`).pipe(
-      map(res => res.data.articles.map(SerializeNewsApiNew)),
+      map(res => res.data.articles.map(this.serializerHelper.SerializeNewsApiNew)),
       catchError(() => {
         return throwError(new ServiceUnavailableException('News API does not respond'));
       }),
